474a2e1d0dcfb3c954b72673e233d7c367d8546e
Sean <sean.o@hotmail.com>
2021-08-01 09:41:08 +0900

    styling sizes

--- public/main.js
+++ public/main.js
@@ -6,10 +6,21 @@ function init() {
     darkToggle.addEventListener('input', () => {
         if (darkToggle.checked === true) {
             document.documentElement.classList.add('dark');
+            localStorage.theme = 'dark';
         } else {
-            document.documentElement.classList.remove('dark')
+            document.documentElement.classList.remove('dark');
+            localStorage.theme = 'light';
         }
     })
+
+    // recommended Tailwind JS to control dark and light themes
+    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+        document.documentElement.classList.add('dark');
+        darkToggle.checked = true;
+    } else {
+        documentElement.classList.remove('dark');
+        darkToggle.checked = false;
+    }
     
 }
 
