5b91770817466afe711d521e09cd3276f5e33052
Sean <sean.o@hotmail.com>
2021-08-03 15:10:48 +0900

    built structure of create / accessibility

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,14 @@
 module.exports = {
     purge: [],
+    /*
+
+    REPLACE PURGE [] WITH THE BELOW WHEN BUILDING FOR PRODUCTION
+
+    {
+      enabled: true,
+      content: ['./public/*.html', './public/*.js'],
+    },
+    */
     darkMode: 'class', // or 'media' or 'class'
     theme: {
       extend: {
