83b93c69c6316322b99d6ca608bb83e6a09032b1
Sean <sean.o@hotmail.com>
2021-08-12 17:09:07 +0900

    merge

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 module.exports = {
     purge: [],
     /*
@@ -32,42 +31,4 @@ module.exports = {
       transitionProperty: ['hover', 'focus'],
     },
     plugins: [],
-=======
-module.exports = {
-    purge: {
-      enabled: true,
-      content: ['./public/*.html', './public/*.js'],
-    },
-    /*
-
-    REPLACE PURGE [] WITH THE BELOW WHEN BUILDING FOR PRODUCTION
-
-    {
-      enabled: true,
-      content: ['./public/*.html', './public/*.js'],
-    },
-    */
-    darkMode: 'class', // or 'media' or 'class'
-    theme: {
-      extend: {
-        colors: {
-          primary: '#FF6363',
-          secondary: {
-            100: '#E2E2D5',
-            200: '#888883',
-          }
-        },
-        fontFamily: {
-          body: ['Nunito']
-        }
-      },
-    },
-    variants: {
-      extend: {
-        animation: ['hover'],
-      },
-      transitionProperty: ['hover', 'focus'],
-    },
-    plugins: [],
->>>>>>> 08013abcff86057eb084a3dd49e2852ccb86b7f2
   }
