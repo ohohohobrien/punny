52cdccb8065a24f92fbfbc4282025158961b50e8
Sean <sean.o@hotmail.com>
2021-08-15 20:39:40 +0900

    added search page tempalte

--- public/search.js
+++ public/search.js
@@ -25,30 +25,19 @@ function init() {
         darkToggle.checked = false;
         document.getElementsByTagName('meta')["theme-color"].content = "#FBBF24";
     }
-
-    // Get the input field
-    const searchField = document.getElementById("searchBar");
-    const searchButton = document.getElementById("searchButton");
-
-    // Allow the input field to be used by pressing enter
-    searchField.addEventListener("keyup", function(event) {
-        // Number 13 is the "Enter" key on the keyboard
-        if (KeyboardEvent.keyCode === 13) {
-            event.preventDefault();
-            searchButton.click();
-        } else if (event.keyCode === 13) {
-            // keyCode is deprecated - therefore using both KeyboardEvent and event.keyCode
-            // Cancel the default action, if needed
-            event.preventDefault();
-            // Trigger the button element with a click
-            searchButton.click();
-        }
-    });
-
-    searchButton.addEventListener('click', () => {
-        const searchKeywords = searchField.value;
-        alert(searchKeywords);
-        window.location.href = './search.html';
-    });
     
+}
+
+function scrollToTargetAdjusted(elementId){ 
+    setTimeout(function() {
+        const element = document.getElementById(`${elementId}`);
+        const headerOffset = 120;
+        const elementPosition = element.getBoundingClientRect().top;
+        const offsetPosition = elementPosition - headerOffset;
+    
+        window.scrollTo({
+             top: offsetPosition,
+             behavior: "smooth"
+        });
+    }, 300); 
 }
